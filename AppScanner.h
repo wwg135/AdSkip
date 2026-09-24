41fcba9258d4d1b141afbd680026b160f7365864
wwg135 <105048516+wwg135@users.noreply.github.com>
2026-09-24 13:47:31 +0800

    Update AppScanner.h

--- AppScanner.h
+++ AppScanner.h
@@ -1,13 +1,15 @@
 #import <Foundation/Foundation.h>
 
 @interface ADSkipApp : NSObject
+
 @property(nonatomic, copy) NSString *bundleID;
 @property(nonatomic, copy) NSString *displayName;
-@property(nonatomic, copy) NSString *path;
 @property(nonatomic, copy) NSString *type;
-@property(nonatomic, assign, getter=isEnabled) BOOL enabled;
+
 @end
 
 @interface AppScanner : NSObject
+
 + (NSArray<ADSkipApp *> *)scanApplications;
+
 @end
